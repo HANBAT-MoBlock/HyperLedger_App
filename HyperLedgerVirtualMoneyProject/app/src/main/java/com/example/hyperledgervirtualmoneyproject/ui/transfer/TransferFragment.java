3c1ee8b9c696286843eeadda99e13e0cc8104c89
CYC-ChoiYoungChang <choiyc1446@gmail.com>
2022-09-19 14:00:07 +0900

    feature: 전송실패시에도 alertDialog 출력

--- HyperLedgerVirtualMoneyProject/app/src/main/java/com/example/hyperledgervirtualmoneyproject/ui/transfer/TransferFragment.java
+++ HyperLedgerVirtualMoneyProject/app/src/main/java/com/example/hyperledgervirtualmoneyproject/ui/transfer/TransferFragment.java
@@ -2,25 +2,30 @@ package com.example.hyperledgervirtualmoneyproject.ui.transfer;
 
 import static android.content.ContentValues.TAG;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.hyperledgervirtualmoneyproject.API.UserApi;
@@ -32,19 +37,18 @@ import com.example.hyperledgervirtualmoneyproject.DTO.UserGetAssetDTO;
 import com.example.hyperledgervirtualmoneyproject.DTO.UserTransferDTO;
 import com.example.hyperledgervirtualmoneyproject.DTO.UserTransferRequestDTO;
 import com.example.hyperledgervirtualmoneyproject.LoadingDialog;
+import com.example.hyperledgervirtualmoneyproject.MainActivity;
 import com.example.hyperledgervirtualmoneyproject.R;
 import com.example.hyperledgervirtualmoneyproject.databinding.FragmentTransferBinding;
-import com.example.hyperledgervirtualmoneyproject.ui.home.CoinListView.CoinData;
-import com.example.hyperledgervirtualmoneyproject.ui.home.CoinListView.CoinLIstAdapter;
+import com.example.hyperledgervirtualmoneyproject.ui.home.HomeFragment;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-import org.w3c.dom.Text;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -169,8 +173,10 @@ public class TransferFragment extends Fragment {
      * @param userTransferRequestDTO the user transfer request dto
      */
     public void coinTransferService(View v, String jwt, UserTransferRequestDTO userTransferRequestDTO){
-        Snackbar snackbar = Snackbar.make(v, "전송중...", 10000000);
-        snackbar.show();
+        ProgressDialog progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("전송중입니다..");
+        progressDialog.show();
+
 
         //step1
         Retrofit retrofit = new Retrofit.Builder()
@@ -179,7 +185,6 @@ public class TransferFragment extends Fragment {
                 .build();
 
         UserTradeApi service = retrofit.create(UserTradeApi.class);
-
         Call<UserTransferDTO> call = service.transfer(jwt, userTransferRequestDTO);
 
         //step2
@@ -189,13 +194,32 @@ public class TransferFragment extends Fragment {
                 //step3
                 if(response.isSuccessful()){
                     UserTransferDTO result = response.body();
-                    snackbar.dismiss();
-                    Snackbar.make(v, "전송 완료했습니다.", 2000).show();
+                    progressDialog.dismiss();
                     Log.d(TAG, "onResponse: 성공, 결과 \n" + result.toString());
+
+                    AlertDialog.Builder successDialog = new AlertDialog.Builder(getContext());
+                    successDialog.setMessage("전송이 완료되었습니다. 홈 화면으로 돌아갑니다.")
+                            .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    MainActivity mainActivity = (MainActivity) getActivity();
+                                    mainActivity.getSupportFragmentManager().beginTransaction()
+                                            .replace(R.id.container, new HomeFragment()).commit();
+                                }
+                            }).create().show();
                 }else{
-                    snackbar.dismiss();
-                    Snackbar.make(v, "전송에 살패했습니다.", 2000).show();
+                    progressDialog.dismiss();
                     Log.d(TAG, "onResponse: 실패");
+                    AlertDialog.Builder successDialog = new AlertDialog.Builder(getContext());
+                    successDialog.setMessage("전송에 실패했습니다. 홈 화면으로 돌아갑니다.")
+                            .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    MainActivity mainActivity = (MainActivity) getActivity();
+                                    mainActivity.getSupportFragmentManager().beginTransaction()
+                                            .replace(R.id.container, new HomeFragment()).commit();
+                                }
+                            }).create().show();
                     try {
                         //exception
                         String errorMessage = response.errorBody().string();
@@ -213,8 +237,8 @@ public class TransferFragment extends Fragment {
 
             @Override
             public void onFailure(Call<UserTransferDTO> call, Throwable t) {
-                snackbar.dismiss();
-                Snackbar.make(v, "서버 연결에 실패했습니다.", 2000).show();
+                progressDialog.dismiss();
+                Toast.makeText(getContext(), "서버 연결에 실패했습니다.", Toast.LENGTH_SHORT).show();
                 Log.d(TAG,"onFailure" + t.getMessage());
             }
         });
